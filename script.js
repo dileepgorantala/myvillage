648b88fa2a3d32eebc92f9ce83ad823318696fcf
dileepgorantala <107741210+dileepgorantala@users.noreply.github.com>
2023-05-25 11:25:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Optional JavaScript code for contact form submission handling
+const form = document.getElementById('contact-form');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    // Code for form submission handling goes here
+    // You can use AJAX or any other technique to send the form data to the server
+});
